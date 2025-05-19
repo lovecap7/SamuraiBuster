37aeaae3497f499881b7a6b25e83e3342fecd662
YukiCreative <YukiNakahiraCreative@gmail.com>
2025-05-19 18:57:15 +0900

    プレイヤーのスキルを作りかける
    あとプレイヤーが攻撃するときに移動しないように。ただし移動方向を入力していれば攻撃が発生する前に少しだけ移動できるようにした

--- SamuraiBuster/Assets/Nakahira/Player/Fighter.cs
+++ SamuraiBuster/Assets/Nakahira/Player/Fighter.cs
@@ -4,6 +4,15 @@ public class Fighter : PlayerBase
 {
     [SerializeField] GameObject m_katana;
     CapsuleCollider m_katanaCollider;
+    // 先行して入力で
+    bool m_isAttackInput = false;
+
+    Vector3 kDodgeForce = new(0,0,5.0f);
+
+    const int kDodgeInterval = 60;
+    const int kAttackInterval = 30;
+
+    int m_dodgeTimer = 0;
 
     // Start is called before the first frame update
     protected override void Start()
@@ -18,11 +27,26 @@ public class Fighter : PlayerBase
     protected override void Update()
     {
         base.Update();
+
+        // タイマー進めて値も制限
+        ++m_dodgeTimer;
+        if (m_dodgeTimer > kDodgeInterval) m_dodgeTimer = kDodgeInterval;
     }
 
     public override void Attack()
     {
+        // 刀を振る
         //m_anim.SetBool("Attaking", true);
+    }
+
+    public override void Skill()
+    {
+        // ドッジロール
+        if (m_dodgeTimer < kDodgeInterval) return;
+
+        m_rigid.AddForce(transform.rotation*kDodgeForce, ForceMode.Impulse);
+
+        m_dodgeTimer = 0; 
     }
 
     public void EnableKatanaCol()
