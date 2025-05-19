102d1dd300be374e869826502e473b96f7d87785
suegin <166367284+suegin@users.noreply.github.com>
2025-05-19 12:13:37 +0900

    up

--- SamuraiBuster/Assets/Suehara/SelectScene/PointerController.cs
+++ SamuraiBuster/Assets/Suehara/SelectScene/PointerController.cs
@@ -7,15 +7,19 @@ public class PointerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject gameObject = GameObject.Find("canvas");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("KeyCode.LeftArrow"))
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-
+            this.transform.position += new Vector3(-300.0f, 0.0f, 0.0f);//Lerp‚Å‚â‚è‚½‚¢
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.transform.position += new Vector3(300.0f, 0.0f, 0.0f);//Lerp‚Å‚â‚è‚½‚¢
         }
     }
 }
