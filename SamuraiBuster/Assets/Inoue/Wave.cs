59b573dbf5385ca6538aac7dfac3b11b354fb75b
lovecap7 <166367138+lovecap7@users.noreply.github.com>
2025-05-22 22:31:59 +0900

    up

---
+++ SamuraiBuster/Assets/Inoue/Wave.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+    //敵キャラ
+    [SerializeField] private GameObject wave;
+    private GameObject[] enemies;
+    //ゲームオブジェクトが0になったらtrue
+    private bool m_isWaveEnd = false;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 子オブジェクト達を入れる配列の初期化
+        enemies = new GameObject[wave.transform.childCount];
+        for (int i = 0; i < enemies.Length; ++i)
+        {
+            enemies[i] = wave.transform.GetChild(i).gameObject;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //死んだエネミーの数
+        int countDeadEnemy = 0;
+        for(int i = 0; i < enemies.Length; ++i)
+        {
+            if (enemies[i] == null)
+            {
+                countDeadEnemy++;
+            }
+        }
+        //すべて倒したら
+        if(countDeadEnemy >= enemies.Length)
+        {
+            m_isWaveEnd = true;
+        }
+    }
+    public bool GetIsWaveEnd()
+    {
+        return m_isWaveEnd;
+    }
+}
--- SamuraiBuster/Assets/Inoue/WaveController.cs
+++ SamuraiBuster/Assets/Inoue/WaveController.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class WaveController : MonoBehaviour
 {
-    //
+    //Wave1
+    [SerializeField] private Wave m_wave1;
+    //Wave2
+    [SerializeField] private Wave m_wave2;
+    //Wave3
+    [SerializeField] private Wave m_wave3;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Wave1Ç™èIÇÌÇ¡ÇΩÇ»ÇÁ
+        if(m_wave1.GetIsWaveEnd())
+        {
+            Debug.Log("Wave1èIóπ");
+        }
     }
 }
