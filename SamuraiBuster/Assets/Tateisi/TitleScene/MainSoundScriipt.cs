310652c84ece852a46ccd55383e0f240c1f14cb0
kuru <166367588+kiryo06@users.noreply.github.com>
2025-06-06 14:27:48 +0900

    up

--- SamuraiBuster/Assets/Tateisi/TitleScene/MainSoundScriipt.cs
+++ SamuraiBuster/Assets/Tateisi/TitleScene/MainSoundScriipt.cs
@@ -4,24 +4,36 @@ using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-[DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+//[DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
 public class MainSoundScriipt : MonoBehaviour
 {
     // 自分が既に存在しているかを確認するためのフラグ  
     private static bool isInstanceExisnt = false;
 
-    private void Update()
+    public void Awake()
     {
+        // 既に存在している場合は、オブジェクトを破棄して終了  
         if (isInstanceExisnt)
         {
-            if (SceneManager.GetActiveScene().name == "StageScene")
-            {
-                Destroy(this.gameObject); // 自分自身を破棄して終了  
-                return;
-            }
+            Destroy(this.gameObject);
+            return;
         }
         // 存在することを記録  
         isInstanceExisnt = true;
+        // シーンを遷移してもオブジェクトを破棄しないようにする  
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+    private void Update()
+    {
+        if ((SceneManager.GetActiveScene().name == "Stage1Scene") ||
+            (SceneManager.GetActiveScene().name == "Stage2Scene") ||
+            (SceneManager.GetActiveScene().name == "Stage3Scene"))
+        {
+            Destroy(this.gameObject); // 自分自身を破棄して終了  
+            return;
+        }
         // シーンを遷移してもオブジェクトを破棄しないようにする  
         DontDestroyOnLoad(this.gameObject);
     } // 修正: Update メソッドの閉じ括弧を追加  
